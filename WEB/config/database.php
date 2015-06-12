255d675192e8eb0e8e39077d87ab1aa7abc87cef
freebsd user <magicteacher95@gmail.com>
2015-06-12 04:00:05 +0000

    connected with mysql database

--- WEB/config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	define(DB_NAME,'');
-	define(DB_HOST,'');
-	define(DB_UID,'');
-	define(DB_PWD,'');
-
-?>
--- WEB/login.php
+++ WEB/login.php
@@ -1,8 +1,26 @@
 <?php
+//	include_once "config/database.php"
 
-	$consumerno = $_POST['uid'];
-	$passwd = $_POST['pwd'];
+//	$consumerno = $_POST['uid'];
+//	$passwd = $_POST['pwd'];
 	
-	
+	//conection: 
+	$conn = mysql_connect("localhost:3306","spechide","password;") or die("Error " . mysqli_error($link)); 	
+//echo $conn;
+	mysql_select_db("spechide");	
+
+	//consultation: 
+
+	$query = "DESC gcm_users"; 
+
+	//execute the query. 
+
+	$result = mysql_query($query,$conn) or die("Error in the consult.." . mysql_error($conn));
+
+	//display information: 
+
+	while($row = mysql_fetch_array($result)) { 
+		 echo $row["name"] . "<br>"; 
+	} 
 
 ?>
