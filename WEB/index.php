50c82c76b725de3f1c6ac370cfb72ca76a667cf0
Shrimadhav U K <magicteacher95@gmail.com>
2015-06-20 15:03:34 +0530

    copied code from http://simsarulhaqv.github.io

--- WEB/index.php
+++ WEB/index.php
@@ -2,56 +2,85 @@
 session_start();
 ?>
 
-<!DOCTYPE html>
 
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Smart Water Meter</title>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Smart Water Meter</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Make sure the page is responsive --> 
+<!--Step 1--> 
+       
+        <!-- Touch func --> 
+		
+		<link rel="stylesheet" href="css/main.css"/>
+		<link rel="stylesheet" href="css/normalize.min.css"/>
+		<link rel="stylesheet" href="css/font-awesome.css">
+		
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/style_01.css">
+        <!-- <link rel="stylesheet" href="css/stylev2.css"> -->		
+		
+		
+		<script type="text/javascript" src="js/modernizr.js"> </script>
+		<script type="text/javascript" src="js/jquery.js"> </script>
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimal-ui">
 
-<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900|Quicksand:400,700|Questrial" rel="stylesheet" />
-<link href="default.css" rel="stylesheet" type="text/css" media="all" />
-<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
 
-<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
-<link href="normalize.css" rel="stylesheet" type="text/css" media="all" />
-</head>
-<body>
-<div id="header-wrapper">
-	<div id="header" class="container">
-		<div id="logo">
-        	<span class="icon icon-cog"></span>
-			<h1><a href="#">Smart Water Meter</a></h1>
-		</div>
-		<div id="menu">
-			<ul>
-				<li><a href="usage.php" accesskey="1" title="">Statistics</a></li>
-				
-				<li class="active"><a href="index.php" accesskey="3" title="">Home</a></li>
-				
-				<li><a href="status.php" accesskey="5" title="">Status</a></li>
-			</ul>
-		</div>
-	</div>
-</div>
-<div class="wrapper">
+    </head>
+    <body>
+
+	
+	<header id ="home" class="wrap clearfix" role="banner">
+
+        <nav role="navigation" > 
+
+            <div class="navicon closed"><i class="fa fa-navicon">
+                </i> </div>
+            <ul class="navmenu" id="opennav">
+                
+                <li><a href="usage.php">Statistics</a></li>
+                <li><a href="index.php" class="active">Home</a></li>
+                <li><a href="status.php">Status</a></li>
+                
+            </ul>
+        </nav>
+
+        <div class="logo_title">
+            <img src="img/image.jpg" alt="logo" width="96"  class="logo">
+            
+        </div>
+        <div class="name">
+			<h3>Smart Water Meter</h3>
+			<h6>an application to monitor the water usage of a household remotely</h6>
+        </div>
+
+
+
+     </header>
+	 
+	 <main role="main" class="clearfix">
+	 
 	
 	
-	<div id="welcome" class="container">
- <!--   	
-<div class="title">
-	  <h2>Welcome to our website</h2>
-		</div> -->
-		
-		
-		
-		
-		<?php
+     <section id="about" class="clearfix wrap">
+
+        
+        
+        <div id="space"></div>
+
+        <!--Adding info about workshops! -->
+
+        <div class="point">
+          
+        </div>
+
+
+         <div class="point">
+       
+				<?php
 
   if(isset($_POST['submit'])) {
 
@@ -105,43 +134,49 @@ session_start();
 		echo "</form>";
   }
 ?>
+			
+        </div>
 
+
+
+         <div class="point">
+        
+        </div>
+
+
+        		
+	</main>
+	
+  
 		
-</div>
 	
-	
-</div>
+	<!-- What about a footer now? -->
+    <div class="footer" >
+    <footer role="complementary" class="clearfix wrap">
 
-</body>
-</html>
-<div id="footer">
-	<div class="container">
-		<div class="fbox1">
-		<span class="icon icon-map-marker"></span>
-			<span>Module No.T-7, 7th Floor, Thejaswini Building ,Technopark, Technopark Campus,
-			<br /> Thiruvananthapuram, Kerala 695581</span>
-		</div>
-		<div class="fbox1">
-			<span class="icon icon-phone"></span>
-			<span>
-				Telephone: +91 471 270 0012
-			</span>
-		</div>
-		<div class="fbox1">
-			<span class="icon icon-envelope"></span>
-			<span> info@icfoss.in </span>
-		</div>
-	</div>
-</div>
-<div id="copyright">
-	<p>&copy; All rights reserved.</p>
-	<ul class="contact">
-		<li><a href="https://twitter.com/fosskerala" class="icon icon-twitter"><span>Twitter</span></a></li>
-		<li><a href="https://www.facebook.com/icfossbootcamp" class="icon icon-facebook"><span>Facebook</span></a></li>
-		<!--<li><a href="#" class="icon icon-dribbble"><span>Pinterest</span></a></li>-->
-		<li><a href="https://plus.google.com/102174442408221199476/" class="icon icon-tumblr"><span>Google+</span></a></li>
-		<!--<li><a href="#" class="icon icon-rss"><span>Pinterest</span></a></li>-->
-	</ul>
-</div>
-</body>
+        <div class="copyright">&copy; 2015. All Rights Reserved.</div>
+        <div class="social"> 
+
+            <div class="icon">
+                <a href="https://twitter.com/fosskerala" target="_blank"><i class="fa fa-twitter-square"></i></a>
+            </div>
+
+            <div class="icon">
+                <a href="https://www.facebook.com/icfossbootcamp" target="_blank"><i class="fa fa-facebook-square"></i></a>
+            </div>
+
+            <div class="icon">
+                <a href="#" target="_blank"><i class="fa fa-phone-square"></i></a>
+            </div>
+
+        </div>
+
+    </footer>
+    </div>
+
+	<script src="js/rwd.js"></script> 
+	
+    </section>	
+
+ </body>
 </html>
--- WEB/status.php
+++ WEB/status.php
@@ -2,53 +2,98 @@
 session_start();
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Smart Water Meter</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Make sure the page is responsive --> 
+<!--Step 1--> 
+       
+        <!-- Touch func --> 
+		
+		<link rel="stylesheet" href="css/main.css"/>
+		<link rel="stylesheet" href="css/normalize.min.css"/>
+		<link rel="stylesheet" href="css/font-awesome.css">
+		
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/style_01.css">
+        <!-- <link rel="stylesheet" href="css/stylev2.css"> -->		
+		
+		
+		<script type="text/javascript" src="js/modernizr.js"> </script>
+		<script type="text/javascript" src="js/jquery.js"> </script>
 
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Smart Water Meter</title>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimal-ui">
 
-<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900|Quicksand:400,700|Questrial" rel="stylesheet" />
-<link href="default.css" rel="stylesheet" type="text/css" media="all" />
-<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
-<link href="normalize.css" rel="stylesheet" type="text/css" media="all" />
-<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+    </head>
+    <body>
 
-</head>
-<body>
-<div id="header-wrapper">
-	<div id="header" class="container">
-		<div id="logo">
-        	<span class="icon icon-cog"></span>
-			<h1><a href="#">Smart Water Meter</a></h1>
-		</div>
-		<div id="menu">
-			<ul>
-				<li><a href="usage.php" accesskey="1" title="">Statistics</a></li>
+	
+	<header id ="home" class="wrap clearfix" role="banner">
+
+        <nav role="navigation" > 
+
+            <div class="navicon closed"><i class="fa fa-navicon">
+                </i> </div>
+            <ul class="navmenu" id="opennav">
+                
+                <li><a href="usage.php">Statistics</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="status.php" class="active">Status</a></li>
+               
+            </ul>
+        </nav>
+
+        <div class="logo_title">
+            <img src="img/image.jpg" alt="logo" width="96"  class="logo">
+            
+        </div>
+        <div class="name">
+			<h3>Smart Water Meter</h3>
+			<h6>an application to monitor the water usage of a household remotely</h6>
+        </div>
+
+
+
+     </header>
+	 
+	 <main role="main" class="clearfix">
+	 
+	
+	
+     <section id="about" class="clearfix wrap">
+
+        
+        
+        <div id="space"></div>
+
+        <!--Adding info about workshops! -->
+
+        <div class="point">
+            <h2><i class="fa fa-code"></i>flow meter</h2>
+            <?php
 				
-				<li><a href="index.php" accesskey="3" title="">Home</a></li>
-				
-				<li class="active"><a href="status.php" accesskey="5" title="">Status</a></li>
-			</ul>
-		</div>
-	</div>
-</div>
-<div class="wrapper">
-	
-	
-	
-	<div id="three-column" class="container">
-		<div><span class="arrow-down"></span></div>
-		<div id="tbox1">
-			<div class="title">
-				<h2>flow meter</h2>
-			</div>
+				if(isset($_SESSION['name'])){
+					$uid = $_SESSION['name'];
+					if($uid){				// admin session id is zero
+						echo "logged in as " . $uid . "with usage 0 litres";
+					}
+					else{
+						echo "logged in as ADMINISTRATOR. can see all usages";
+					}
+				}
+				else{
+					echo "not logged in.";
+				}
+			?>
+        </div>
+
+
+         <div class="point">
+            <h2><i class="fa fa-code"></i>microcontroller</h2>
 			<?php
 				
 				if(isset($_SESSION['name'])){
@@ -64,13 +109,12 @@ session_start();
 					echo "not logged in.";
 				}
 			?>
-			
-		<!--	<p>Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna. Duis pretium velit ac suscipit mauris. Proin eu wisi suscipit nulla suscipit interdum.</p>
-			<a href="#" class="button">Learn More</a> --> </div>
-		<div id="tbox2">
-			<div class="title">
-				<h2>microcontroller</h2>
-			</div>
+        </div>
+
+
+
+         <div class="point">
+            <h2><i class="fa fa-code"></i>GPRS Module</h2>
 			<?php
 				
 				if(isset($_SESSION['name'])){
@@ -86,62 +130,40 @@ session_start();
 					echo "not logged in.";
 				}
 			?>
-			
-		<!--	<p>Proin eu wisi suscipit nulla suscipit interdum. Nullam non wisi a sem semper suscipit eleifend. Donec mattis libero eget urna. Duis  velit ac mauris.</p>
-			<a href="#" class="button">Learn More</a> --> </div>
-		<div id="tbox3">
-			<div class="title">
-				<h2>GPRS module</h2>
-			</div>
-			<?php
-				
-				if(isset($_SESSION['name'])){
-					$uid = $_SESSION['name'];
-					if($uid){				// admin session id is zero
-						echo "logged in as " . $uid . "with usage 0 litres";
-					}
-					else{
-						echo "logged in as ADMINISTRATOR. can see all usages";
-					}
-				}
-				else{
-					echo "not logged in.";
-				}
-			?>
-			
-		<!--	<p>Donec mattis libero eget urna. Duis pretium velit ac mauris. Proin eu wisi suscipit nulla suscipit interdum. Nullam non wisi a sem suscipit  eleifend.</p>
-			<a href="#" class="button">Learn More</a> --> </div>
-	</div>
+        </div>
+
+
+        		
+	</main>
 	
-</div>
-<div id="footer">
-	<div class="container">
-		<div class="fbox1">
-		<span class="icon icon-map-marker"></span>
-			<span>Module No.T-7, 7th Floor, Thejaswini Building ,Technopark, Technopark Campus,
-			<br /> Thiruvananthapuram, Kerala 695581</span>
-		</div>
-		<div class="fbox1">
-			<span class="icon icon-phone"></span>
-			<span>
-				Telephone: +91 471 270 0012
-			</span>
-		</div>
-		<div class="fbox1">
-			<span class="icon icon-envelope"></span>
-			<span> info@icfoss.in </span>
-		</div>
-	</div>
-</div>
-<div id="copyright">
-	<p>&copy; All rights reserved.</p>
-	<ul class="contact">
-		<li><a href="https://twitter.com/fosskerala" class="icon icon-twitter"><span>Twitter</span></a></li>
-		<li><a href="https://www.facebook.com/icfossbootcamp" class="icon icon-facebook"><span>Facebook</span></a></li>
-		<!--<li><a href="#" class="icon icon-dribbble"><span>Pinterest</span></a></li>-->
-		<li><a href="https://plus.google.com/102174442408221199476/" class="icon icon-tumblr"><span>Google+</span></a></li>
-		<!--<li><a href="#" class="icon icon-rss"><span>Pinterest</span></a></li>-->
-	</ul>
-</div>
-</body>
+ 	
+	<!-- What about a footer now? -->
+    <div class="footer" >
+    <footer role="complementary" class="clearfix wrap">
+
+        <div class="copyright">&copy; 2015. All Rights Reserved.</div>
+        <div class="social"> 
+
+            <div class="icon">
+                <a href="https://twitter.com/fosskerala" target="_blank"><i class="fa fa-twitter-square"></i></a>
+            </div>
+
+            <div class="icon">
+                <a href="https://www.facebook.com/icfossbootcamp" target="_blank"><i class="fa fa-facebook-square"></i></a>
+            </div>
+
+            <div class="icon">
+                <a href="#" target="_blank"><i class="fa fa-phone-square"></i></a>
+            </div>
+
+        </div>
+
+    </footer>
+    </div>
+
+	<script src="js/rwd.js"></script> 
+	
+    </section>	
+
+ </body>
 </html>
