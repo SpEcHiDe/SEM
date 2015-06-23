af0d79eb3abcecfb9317fe89efdf13d07dafa136
Shrimadhav U K <magicteacher95@gmail.com>
2015-06-23 10:39:33 +0530

    implemented PHP MySQL connection

--- WEB/save_log.php
+++ WEB/save_log.php
@@ -1,21 +1,28 @@
 <?php
-        $File = "data/value.log";
-        //This is the text file we keep our count in, that we just made
-                $handle = fopen($File, 'r+') ;
-        //Here we set the file, and the permissions to read plus write
-                $data = fread($handle, 512) ;
-        //Actully get the count from the file
-                $recv = $_GET['value'];
-                $date = date('Y-m-d\TH:i:sO');
-                $count = $date . " # " . $recv . PHP_EOL . $data;
-        //Add the new visitor to the count
-        //      print "<font size=2>Number of times visited : " . $count . "</font><br /><hr /> ";
-        //Prints the count on the page
-                fseek($handle, 0) ;
-        //Puts the pointer back to the begining of the file
-                fwrite($handle, $count) ;
-        //saves the new count to the file
-                fclose($handle) ;
-        //closes the file
+		
+		$mysql_host = "mysql2.000webhost.com";
+		$mysql_database = "a7089677_iot";
+		$mysql_user = "a7089677_iot";
+		$mysql_password = "pASSword2014;";
+        
+        $recv = $_POST['value'];
+        $consno = $_POST['uid'];
+        
+        try {
+			$conn = new PDO("mysql:host=$mysql_host;dbname=$mysql_database", $mysql_user, $mysql_password);
+			// set the PDO error mode to exception
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sql = "INSERT INTO data (consno, consumption) VALUES ('".$consno."', '".$recv."')";
+			// use exec() because no results are returned
+			$conn->exec($sql);
+			echo "New record created successfully";
+		}
+		catch(PDOException $e)
+		{
+			echo $sql . "<br>" . $e->getMessage();
+		}
+	
+		$conn = null;
 
+        
 ?>
